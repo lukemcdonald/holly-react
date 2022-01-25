f82daf0b0783252a591a906f411412f7a7a4e4f3
Luke McDonald <thelukemcdonald@gmail.com>
2022-01-25 15:10:38 -0600

    Replace "classnames" with 'clsx"
    This is a more lightweight package that does the same thing I need it to do.

--- src/components/forms/NewsletterForm.js
+++ src/components/forms/NewsletterForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import classnames from 'classnames'
+import classnames from 'clsx'
 
 export default function NewsletterForm({ className, onSubmit, submitBtn }) {
 	const [email, setEmail] = React.useState('')
