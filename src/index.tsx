8c7c347df949d5ea44ac88f8d0441f40165654f2
souvik666 <peacedynasty0@gmail.com>
2023-01-18 12:47:28 +0530

    feat: Add TypeScript #13

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './app'
 
 import './assets/styles.css'
-import App from './app'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
