91ad34e1d5f70a85eb8e02c00d8fbce3a781f197
Luke McDonald <thelukemcdonald@gmail.com>
2023-01-22 19:56:49 -0600

    fix: update vite base to allow gh-pages to work

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  base: './',
+  base: '/holly-react/',
   build: {
     outDir: '../dist',
   },
