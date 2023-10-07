6c655e3db78193db9045f590c71974da40704127
Luke McDonald <lukemcdonald@users.noreply.github.com>
2023-10-07 12:46:58 -0500

    fix: update footer and entry image spacing (#26)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="bg-primary-400 text-sm leading-5 tracking-normal text-white lg:bg-transparent lg:text-gray-400">
       <div className="mx-auto w-full max-w-6xl px-6">
-        <div className="relative flex flex-wrap py-10 lg:justify-between">
+        <div className="relative flex flex-wrap lg:justify-between">
           <NavLinks className="mb-6 inline-flex w-full flex-none justify-center lg:order-1 lg:mb-0 lg:flex lg:w-1/2 lg:justify-end" />
           <SocialLinks className="mb-6 inline-flex w-full flex-none justify-center lg:flex lg:justify-end" />
           <div className="mb-6 inline-flex w-full flex-none justify-center lg:w-1/2 lg:justify-start">
