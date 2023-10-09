50ea82535a6dda59073a995cf7b49d82849c75b8
Luke McDonald <thelukemcdonald@gmail.com>
2023-10-08 19:21:11 -0500

    fix: improve footer responsive styles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,14 +3,14 @@ import { SocialLinks } from '@/components/social-links'
 
 function Footer() {
   return (
-    <footer className="bg-primary-400 text-sm leading-5 tracking-normal text-white lg:bg-transparent lg:text-gray-400">
+    <footer className="bg-primary-400 py-6 text-sm leading-5 tracking-normal text-white lg:bg-transparent lg:text-gray-400">
       <div className="mx-auto w-full max-w-6xl px-6">
-        <div className="relative flex flex-wrap lg:justify-between">
-          <Menu className="mb-6 inline-flex w-full flex-none justify-center gap-4 lg:order-1 lg:mb-0 lg:flex lg:w-1/2 lg:justify-end" />
-          <SocialLinks className="mb-6 inline-flex w-full flex-none justify-center lg:flex lg:justify-end" />
-          <div className="mb-6 inline-flex w-full flex-none justify-center lg:w-1/2 lg:justify-start">
-            &copy; Holly
-          </div>
+        <div className="relative flex flex-col items-center gap-6 lg:flex-row lg:items-end lg:justify-between">
+          <nav className="flex flex-col items-center gap-6 lg:order-1 lg:items-end">
+            <SocialLinks className="flex gap-4" />
+            <Menu className="flex gap-4" />
+          </nav>
+          <div className="">&copy; Holly</div>
         </div>
       </div>
     </footer>
